5e1faf12b1802e6476acb0389a2dc3c561d10559
Ivan Solomichev <vanijatko.social@gmail.com>
2020-12-07 09:34:22 +0200

    joinGame added

--- contracts/CoinFlip.sol
+++ contracts/CoinFlip.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.5;
 
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
 contract CoinFlip {
+  using SafeMath for uint256;
 
   uint256 public gameMinBet = 1e16; //  0.001 ETH
   uint256 public gameMaxDuration = 1 days;
@@ -9,12 +12,15 @@ contract CoinFlip {
   uint256 public gamesStarted;
   uint256 public gamesFinished;
 
+  mapping(address => uint256) public betTotal;
+  mapping(address => uint256) public withdrawTotal;
+
   //  Running game properties
   bytes32 public runningGameCoinSideHash; //  TODO: clear on finish
   uint256 public runningGameBet;          //  TODO: clear on finish
   uint256 public runningGameTimestamp;    //  TODO: clear on finish
   address public runningGameCreator;      //  TODO: clear on finish
-  address[] public runningGameOpponents;  //  TODO: clear on finish
+  mapping(address => uint8) public runningGameOpponentGuess;  //  TODO: clear on finish
 
   constructor() {}
 
@@ -27,5 +33,17 @@ contract CoinFlip {
     runningGameBet = msg.value;
     runningGameTimestamp = block.timestamp;
     runningGameCreator = msg.sender;
+
+    betTotal[msg.sender] = betTotal[msg.sender].add(msg.value);
+  }
+
+  function joinGame(uint8 _coinSide) external payable {
+    require(runningGameTimestamp > 0, "No running game");
+    require(runningGameOpponentGuess[msg.sender] == 0, "Already joined");
+    require(_coinSide == 1 || _coinSide == 2, "Wrong side"); //  1, 2
+    require(msg.value == runningGameBet, "Wrong bet");
+
+    runningGameOpponentGuess[msg.sender] = _coinSide ;
+    betTotal[msg.sender] = betTotal[msg.sender].add(msg.value);
   }
 }
