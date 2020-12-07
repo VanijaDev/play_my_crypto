181551d8741d4d5295adbf582a6bdb8ce8681e91
Ivan Solomichev <vanijatko.social@gmail.com>
2020-12-07 08:52:35 +0200

    startGame added

--- contracts/CoinFlip.sol
+++ contracts/CoinFlip.sol
@@ -2,5 +2,30 @@
 pragma solidity ^0.7.5;
 
 contract CoinFlip {
+
+  uint256 public gameMinBet = 1e16; //  0.001 ETH
+  uint256 public gameMaxDuration = 1 days;
+
+  uint256 public gamesStarted;
+  uint256 public gamesFinished;
+
+  //  Running game properties
+  bytes32 public runningGameCoinSideHash; //  TODO: clear on finish
+  uint256 public runningGameBet;          //  TODO: clear on finish
+  uint256 public runningGameTimestamp;    //  TODO: clear on finish
+  address public runningGameCreator;      //  TODO: clear on finish
+  address[] public runningGameOpponents;  //  TODO: clear on finish
+
   constructor() {}
+
+  function startGame(bytes32 _coinSideHash) external payable {
+    require(_coinSideHash[0] != 0, "Empty hash");
+    require(runningGameTimestamp == 0, "Already started");
+    require(msg.value > gameMinBet, "value < gameMinBet");
+
+    runningGameCoinSideHash = _coinSideHash;
+    runningGameBet = msg.value;
+    runningGameTimestamp = block.timestamp;
+    runningGameCreator = msg.sender;
+  }
 }
