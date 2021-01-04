1c571f113ca6e092b9ff8537b48e15d5462bb0bb
Ivan Solomichev <vanijatko.social@gmail.com>
2021-01-04 10:08:07 +0200

    PMCRaffle optimized & comments

--- contracts/PMCRaffle.sol
+++ contracts/PMCRaffle.sol
@@ -6,7 +6,6 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 /**
- * @notice ETH only
  * @dev Run when game is played by creator. Acumulates balances & participants of not played games.
  */
 abstract contract PMCRaffle is Ownable {
@@ -20,24 +19,35 @@ abstract contract PMCRaffle is Ownable {
   mapping(address => mapping(address => uint256)) public raffleJackpotWithdrawPending;  //  token => (address => amount), 0x0 - ETH
   mapping(address => mapping(address => uint256)) public raffleJackpotWithdrawn;
   
-  mapping(address => uint256) public raffleJackpot; //  address => amount, 0x0 - ETH
+  mapping(address => uint256) public raffleJackpot; //  token => amount, 0x0 - ETH
   mapping(address => uint256) public raffleJackpotsWonTotal;
 
-  mapping(address => address[]) private raffleParticipants;
+  mapping(address => address[]) private raffleParticipants; //  token => addresses, 0x0 - ETH
   mapping(address => RaffleResult[]) public raffleResults;
 
 
   event CF_RafflePlayed(address token, address indexed winner, uint256 indexed prize);
   event CF_RaffleJackpotWithdrawn(address token, address indexed winner);
 
+  /**
+   * @dev Adds to current raffle.
+   * @param _token Token address.
+   * @param _amount Amount.
+   * @param _player Player address.
+   */
   function addToRaffle(address _token, uint256 _amount, address _player) internal {
     require(_amount > 0, "Wrong amount");
-    require(_player != address(0), "Raffle player 0x0");
+    require(_player != address(0), "Wrong player");
 
     raffleJackpot[_token] = raffleJackpot[_token].add(_amount);
     raffleParticipants[_token].push(_player);
   }
 
+  /**
+   * @dev Adds to current raffle with no player.
+   * @param _token Token address.
+   * @param _amount Amount.
+   */
   function addToRaffleNoPlayer(address _token, uint256 _amount) internal {
     require(_amount > 0, "Wrong amount");
 
@@ -46,6 +56,7 @@ abstract contract PMCRaffle is Ownable {
 
   /**
    * @dev Gets raffle participants.
+   * @param _token Token address.
    * @return Participants list.
    */
   function getRaffleParticipants(address _token) external view returns (address[] memory) {
@@ -54,6 +65,7 @@ abstract contract PMCRaffle is Ownable {
 
   /**
    * @dev Gets raffle participants number.
+   * @param _token Token address.
    * @return Participants number.
    */
   function getRaffleParticipantsNumber(address _token) external view returns (uint256) {
@@ -62,6 +74,7 @@ abstract contract PMCRaffle is Ownable {
 
   /**
    * @dev Gets past raffle results number.
+   * @param _token Token address.
    * @return Results number.
    */
   function getRaffleResultNumber(address _token) external view returns (uint256) {
@@ -69,11 +82,12 @@ abstract contract PMCRaffle is Ownable {
   }
 
   /**
-   * @dev Runs the raffle.
+   * @dev Runs the current raffle.
+   * @param _token Token address.
    */
   function runRaffle(address _token) internal {
     if (raffleJackpot[_token] > 0 && raffleParticipants[_token].length > 0) {
-      uint256 winnerIdx = rand(_token);
+      uint256 winnerIdx = _rand(_token);
       raffleJackpotWithdrawPending[_token][raffleParticipants[_token][winnerIdx]] = raffleJackpotWithdrawPending[_token][raffleParticipants[_token][winnerIdx]].add(raffleJackpot[_token]);
       raffleJackpotsWonTotal[_token] = raffleJackpotsWonTotal[_token].add(raffleJackpot[_token]);
       raffleResults[_token].push(RaffleResult(raffleParticipants[_token][winnerIdx], raffleJackpot[_token]));
@@ -87,14 +101,16 @@ abstract contract PMCRaffle is Ownable {
 
   /**
    * @dev Generates random number
+   * @param _token Token address.
    */
-  function rand(address _token) private view returns(uint256) {
+  function _rand(address _token) private view returns(uint256) {
     require(raffleParticipants[_token].length > 0, "No participants");
     return uint256(keccak256(abi.encodePacked(block.timestamp, raffleJackpot[_token], raffleParticipants[_token].length))) % raffleParticipants[_token].length;
   }
 
   /**
    * @dev Withdraw jackpots for all won raffles.
+   * @param _token Token address.
    */
   function withdrawRaffleJackpotsPending(address _token) external {
     uint256 amountToSend = raffleJackpotWithdrawPending[_token][msg.sender];
@@ -104,10 +120,10 @@ abstract contract PMCRaffle is Ownable {
 
     raffleJackpotWithdrawn[_token][msg.sender] = raffleJackpotWithdrawn[_token][msg.sender].add(amountToSend);
 
-    if (_token == address(0)) {
-      msg.sender.transfer(amountToSend);
-    } else {
+    if (_token != address(0)) {
       ERC20(_token).transfer(msg.sender, amountToSend);
+    } else {
+      msg.sender.transfer(amountToSend);
     }
     
     CF_RaffleJackpotWithdrawn(_token, msg.sender);
