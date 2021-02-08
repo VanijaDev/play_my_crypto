4d0bf1fb5176640859f1cf61f04d11a6aa142206
Ivan Solomichev <vanijatko.social@gmail.com>
2021-02-08 20:44:22 +0200

    comment updated

--- contracts/PMC_IStaking.sol
+++ contracts/PMC_IStaking.sol
@@ -7,7 +7,7 @@ pragma solidity ^0.7.6;
  */
 interface PMC_IStaking {
     /**
-     * @notice Staking Smart Contract uses ETH only as staking reward. ETH stays on Gameplay Smart Contract balance. Staking Smart Contract just keeps track of all the values.
+     * @notice Staking Smart Contract uses ETH only as staking reward.
      * @dev Replenishes Staking Smart Contract with ETH amount.
      */
     function replenishRewardPool() external payable;
