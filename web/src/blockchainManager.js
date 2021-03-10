c0cdff363ce50dfed0210254a59934439b794f19
Ivan Solomichev <vanijatko.social@gmail.com>
2021-03-10 17:03:01 +0200

    added ethers npm

--- web/src/blockchainManager.js
+++ web/src/blockchainManager.js
@@ -1,31 +1,15 @@
+import {
+  ethers
+} from "ethers";
+
 const BlockchainManager = {
-
-  MetaMaskCodes: {
-    userDenied: 4001
-  },
-
-  isInitted: false,
-
-  isEthereum: function () {
-    return (ethereum != null && typeof ethereum !== 'undefined');
-  },
-
-  isMetaMask: function () {
-    return ethereum.isMetaMask;
-  },
-
-  init: async function () {
-    if (window.BlockchainManager.isEthereum()) {
-      ethereum.autoRefreshOnNetworkChange = false;
-      window.BlockchainManager.isInitted = true;
-      //console.log("BlockchainManager - OK");            
-    } else {
-      console.log("BlockchainManager - ERROR");
-    }
+  //  API
+  referralFeesUsedTotal: async function (_gameType) {
+    return new BigNumber(await PromiseManager.referralFeesUsedTotalPromise(_gameType));
   },
 };
 
 
-window.BlockchainManager = BlockchainManager;
+// window.BlockchainManager = BlockchainManager;
 
 export default BlockchainManager;
