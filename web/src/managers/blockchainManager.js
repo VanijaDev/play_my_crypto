f996c0309982765691658dc9e1f2c326abcfc9d6
Ivan Solomichev <vanijatko.social@gmail.com>
2021-03-12 19:06:14 +0200

    removed unused code

--- web/src/managers/blockchainManager.js
+++ web/src/managers/blockchainManager.js
@@ -59,58 +59,7 @@ const BlockchainManager = {
       default:
         return false;
     }
-  },
-
-  //  PMCt
-  pmct_allowance_promise: function (_spender) {
-    return window.BlockchainManager.pmctInst.allowance(window.BlockchainManager.gameInst.address, _spender);
-  },
-
-  // CF
-  cf_betsTotal_promise: function (_address) {
-    return window.BlockchainManager.gameInst.betsTotal(_address);
-  },
-
-  cf_playerStakeTotal_promise: function (_token) {
-    return window.BlockchainManager.gameInst.getPlayerStakeTotal(_token);
-  },
-
-  cf_playerWithdrawedTotal_promise: function (_token) {
-    return window.BlockchainManager.gameInst.getPlayerWithdrawedTotal(_token);
-  },
-
-  cf_playerWithdrawedPMCtTotal_promise: function (_address) {
-    return window.BlockchainManager.gameInst.playerWithdrawedPMCtTotal(_address);
-  },
-
-  cf_gamesParticipatedToCheckPrize_promise: function (_token) {
-    return window.BlockchainManager.gameInst.getGamesParticipatedToCheckPrize(_token);
-  },
-
-  cf_opponentCoinSideForOpponent_promise: function (_address, _gameIdx) {
-    return window.BlockchainManager.gameInst.opponentCoinSideForOpponent(_address, _gameIdx);
-  },
-
-  cf_referralInGame_promise: function (_token, _gameIdx) {
-    return window.BlockchainManager.gameInst.getReferralInGame(_token, _gameIdx);
-  },
-
-  cf_gameInfo_promise: function (_token, _gameIdx) {
-    return window.BlockchainManager.gameInst.gameInfo(_token, _gameIdx);
-  },
-
-  cf_pendingPrizeToWithdraw_promise: function (_token, _maxLoop) {
-    return window.BlockchainManager.gameInst.pendingPrizeToWithdraw(_token, _maxLoop);
-  },
-
-  cf_gamesStarted_promise: function (_token) {
-    return window.BlockchainManager.gameInst.gamesStarted(_token);
-  },
-
-  cf_gamesFinished_promise: function (_token) {
-    return window.BlockchainManager.gameInst.gamesFinished(_token);
-  },
-
+  }
 
 };
 
