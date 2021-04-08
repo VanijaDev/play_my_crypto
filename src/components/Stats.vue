4d573b4ad55a9f6716fd846828a9cf0e721200f6
Ivan Solomichev <vanijatko.social@gmail.com>
2021-04-08 18:48:15 +0300

    updated label Stats -> Staing Stats

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="__content-block __cb_white h-100 d-flex flex-column justify-content-between">
     <div>
-      <h3>{{ $t('stats') }}</h3>
+      <h3>{{ $t('staking_stats') }}</h3>
       <!-- Total staken -->
       <div class="__text_line">
-        <span class="__stats_col_1">{{ $t('total_staken') }}:</span>       
+        <span class="__stats_col_1">{{ $t('total_staken') }}:</span>
         <span class="__stats_col_2"></span>
         <div class="__img_value_block __stats_col_3">
           <img src="/img/logo.svg" height="25"  width="25" alt="ETH">
           <span id="stats_1">{{gUser.stakingData.tokensStaked | formatBalanceShort}}</span>
-          <b-tooltip target="stats_1" custom-class="__tooltip" >{{gUser.stakingData.tokensStaked | formatBalance}}</b-tooltip>  
+          <b-tooltip target="stats_1" custom-class="__tooltip" >{{gUser.stakingData.tokensStaked | formatBalance}}</b-tooltip>
         </div>  
-        <div class="ml-2 __stats_col_4"></div>         
-                   
+        <div class="ml-2 __stats_col_4"></div>
+
       </div>
       <!-- Your stake -->
       <div class="__text_line">
@@ -22,10 +22,10 @@
         <div class="__img_value_block __stats_col_3">
           <img src="/img/logo.svg" height="25"  width="25" alt="ETH">
           <span id="stats_2">{{gUser.stakingData.stake | formatBalanceShort}}</span>
-          <b-tooltip target="stats_2" custom-class="__tooltip" >{{gUser.stakingData.stake | formatBalance}}</b-tooltip>  
+          <b-tooltip target="stats_2" custom-class="__tooltip" >{{gUser.stakingData.stake | formatBalance}}</b-tooltip>
         </div> 
         <!-- Unstake -->
-        <button type="button" class="btn btn-primary __blue_button ml-2 __stats_col_4" :disabled="unStakeDisabled" @click="unstake()">{{ $t('unstake') }}</button>        
+        <button type="button" class="btn btn-primary __blue_button ml-2 __stats_col_4" :disabled="unStakeDisabled" @click="unstake()">{{ $t('unstake') }}</button>
       </div>
     </div>
     
@@ -37,24 +37,24 @@
 
     <HowToStakeModal/>
     <GameFAQStatsModal/>
-  </div> 
+  </div>
 </template>
 
-<script>  
+<script>
   import HowToStakeModal from '@/components/modals/HowToStakeModal.vue';
   import GameFAQStatsModal from '@/components/modals/GameFAQStatsModal.vue';
   export default {
-    name: 'Stats', 
+    name: 'Stats',
     components: {
-      HowToStakeModal, 
+      HowToStakeModal,
       GameFAQStatsModal
     },
     computed: {
       unStakeDisabled() { 
         if (!this.gUser.stakingData.stake) return true
         if (this.gUser.stakingData.stake.lte(0)) return true
-        return false  
-      },       
+        return false
+      },
     },
     methods: {
       unstake() {
@@ -64,14 +64,14 @@
     i18n: {
       messages: {
         en: {
-          stats: 'Stats',
+          staking_stats: 'Staking Stats',
           total_staken: 'Total staken',
           your_stake: 'Your stake',
           unstake: 'Unstake',
           how_to_stake: 'How to stake?',
           game_faq: 'Game FAQ',
-        },          
-      }      
+        },
+      }
     }
   }
 </script>
